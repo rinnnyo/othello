2e2da5731a123814d163b34593d030891ec408ab
rinnnyo <rinnnyo@users.noreply.github.com>
2023-06-15 09:56:49 +0000

    deploy: c5c38a38c8098a3b979f326a75e19e7b4d5863c4

--- _next/static/sisXJ-xc7J5zGkFqkNWCO/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/302c14d0eaad70fa.css","static/chunks/pages/index-3bed05aa406a4e22.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
